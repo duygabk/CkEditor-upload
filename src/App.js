8d6f652ac601c80e0a010c7aac49cce931c7585e
duygabk <45941584+duygabk@users.noreply.github.com>
2020-08-06 17:11:22 +0900

    Add App Menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 // import ProductForm from "./components/admin/ProductForm";
 import AppHeader from "./components/common/AppHeader";
-// import ProductCard from "./components/product/ProductCard";
+import ProductCard from "./components/product/ProductCard";
+import { getAppMenu } from "./utils/axios";
 
 // import Progress from "./components/Progress";
 // import UploadMulty from "./components/UploadMulty";
 
 export default function App() {
+  // first time, load user, menu, display item...
+  useEffect(() => {
+    getAppMenu()
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <div className="container mt-2">
+      <div className="container mt-1">
+        {/* <div className="row"> */}
         <AppHeader />
-        <div className="row">
+        {/* </div> */}
+        <div className="row mt-2">
           <div className="card-columns">
-            {/* <ProductCard productInfo={{}} /> */}
+            <ProductCard productInfo={{}} />
+            <ProductCard productInfo={{}} />
+            <ProductCard productInfo={{}} />
+            <ProductCard productInfo={{}} />
+            <ProductCard productInfo={{}} />
           </div>
         </div>
       </div>
